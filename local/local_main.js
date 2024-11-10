3090569c8ac54ef5e9100ffd77d3e08220593e96
crckfx <crckfx@outlook.com>
2024-11-10 22:22:00 +1100

    add 'local' edition (not run as a module)

---
+++ local/local_main.js
@@ -0,0 +1,7 @@
+// import { Calculator } from "./Calculator.js"
+// import { initKeyboard } from "./keyboard.js";
+// ----------------------------------------------------------------
+// --- Instantiate the Calculator ---
+const calculator = new Calculator(document.getElementById("calculator"));
+initKeyboard(calculator);
+// ----------------------------------------------------------------
