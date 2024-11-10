10e2c8068b9a8259cf18474dbc576a6829df6845
crckfx <crckfx@outlook.com>
2024-11-11 00:25:37 +1100

    fix the wrapping again to centre it using calculator.css

--- index.php
+++ index.php
@@ -7,18 +7,9 @@ include (PUBLIC_URL . 'site_main/header.html');
 require_once(PRIVATE_URL . 'process_module_queries.php'); // we now use functions from this other doc
 if (processModuleQueries('calculator') < 1) {
     // reach here IF no query parameters found. otherwise, processModuleQueries() will handle the rest.
-
-    // echo "<div style='display: flex; align-items: center; flex-direction: column; margin:10px;'>";
     echo "<div class='main-content'>";
-
-    
-
-    echo "<h1>Calculator</h1>";
     include ('module/calculator_mobile.html');
-    
-    
     echo "</div>";
-
 } 
 
 include (PUBLIC_URL . 'site_main/footer.html');
